0a5090c4b51e9be5032b2f3281403f141bc9f386
mixelpixel <mixelpix@mac.com>
2017-05-15 04:46:14 -0400

    ex1

---
+++ ex1/ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello, world!");
+    puts("\tNeat - tabs!");
+    puts("Look: ab\bcd - backspaces");
+    puts("\aDid you hear a bell?");
+    puts("\\ Did a backslah show up?");
+    puts("puts'ing backslashes takes extra work!");
+
+    return 0;
+}
+
