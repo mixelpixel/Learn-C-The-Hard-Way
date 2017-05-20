5f686c3544c00f0cebc14acd89b089d2ca2ed220
mixelpixel <mixelpix@mac.com>
2017-05-20 04:49:10 -0400

    #include <stdio>

--- 01/1.1.c
+++ 01/1.1.c
@@ -1,7 +1,7 @@
-#include <stdio.h> // <------------- no warnings with $ make 1.1
-                   // -------------- or $ CFLAGS="-Wall" make 1.1
-int main(int argc, char *argv[])
-{
+#include <stdio.h> // <--------------- no warnings with $ make 1.1
+                   // ---------------- or $ CFLAGS="-Wall" make 1.1
+int main(int argc, char *argv[]) //--- but warnings with $ CFLAGS="-WCL4" make 1.1
+{                                //--- for unused parameters
     puts("Hello world.");
 
     return 0;
@@ -12,10 +12,11 @@ int main(int argc, char *argv[])
 // #include <stdio.h>
 // int main(int argc, char *argv[]){puts("Hello world.");return 0;}
 
+// int main(void) avoids unused parameter warning with $ CFLAGS="-WCL4" make 1.1
 // #include <stdio.h>
 //
 // int main(void)     // <------------- also no warnings with $ make 1.1
-// {
+// {                  // <------------- or $ CFLAGS="-WCL4" make 1.1
 //     puts("Hello world.");
 //
 //     return 0;
