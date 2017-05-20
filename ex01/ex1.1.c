5e65d14f16a0b6e7fab7e025fe708ae2e3a50eb8
mixelpixel <mixelpix@mac.com>
2017-05-20 05:49:05 -0400

    naming convention change

--- ex01/ex1.1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) // $ CFLAGS='-WCL4' make ex1.2 compile warning
-{
-    puts("Hello world.");
-
-    return 0;
-}
--- ex01/ex1.2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-// no warning messages when compiling: $ CFLAGS='-WCL4' make ex1.2
-int main(void)
-{
-    puts("Hello world.");
-
-    return 0;
-}
