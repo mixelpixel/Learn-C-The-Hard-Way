a1db85b8641e06b3690558261faa039ee14fc594
mixelpixel <mixelpix@mac.com>
2017-05-20 05:00:28 -0400

    extra credit

---
+++ 01/extra_credit/1.c
@@ -0,0 +1,27 @@
+/*
+Extra Credit
+1. Open the ex1 file in your text editor and change or delete random parts.
+   Try running it and see what happens.
+2. Print out 5 more lines of text or something more complex than hello world.
+3. Run man 3 puts and read about this function and many others.
+*/
+
+#include <stdio.h>
+// K&R style of C: not declaring function type, i.e. int main(void):
+main(void)
+{
+    puts("\nHello, world!");
+
+    return 0;
+}
+
+/* warning: TYPE SPECIFIER MISSING
+$  make extra_credit_ex1.1
+cc    1.1.c   -o 1.1
+1.1.c:3:1: warning: type specifier missing, defaults to 'int' [-Wimplicit-int]
+main(void)
+^
+1 warning generated.
+
+Just a warning, defaults to int and compiles. Ta-da!
+*/
