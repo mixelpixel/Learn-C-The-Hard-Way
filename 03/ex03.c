070acd6e46efa751b0a45ab7ab8057d35deec319
mixelpixel <mixelpix@mac.com>
2018-01-23 23:59:34 -0500

    ex03

---
+++ 03/ex03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int age = 43;
+  int height = 70;
+
+  printf("I am %d years old.\n", age);
+  // printf("I am %d years old.\n");
+  // printf("I am %d years old.\n", argv);
+  printf("I am %d inches tall.\n", height);
+
+  return 0;
+}
